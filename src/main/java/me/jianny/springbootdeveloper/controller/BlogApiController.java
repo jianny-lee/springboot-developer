eb7df8742cfaeef39351b1632626ea875b735829
jianny-lee <jianny.lee96@gmail.com>
2025-01-18 17:01:53 +0900

    7장까지 실습

--- src/main/java/me/jianny/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/jianny/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,14 @@ package me.jianny.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.jianny.springbootdeveloper.domain.Article;
 import me.jianny.springbootdeveloper.dto.AddArticleRequest;
+import me.jianny.springbootdeveloper.dto.ArticleResponse;
+import me.jianny.springbootdeveloper.dto.UpdateArticleRequest;
 import me.jianny.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController //HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -24,5 +26,42 @@ public class BlogApiController {
         //요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    //URL경로에서 값 추출
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id){
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    @DeleteMapping("api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable long id){
+        blogService.delete(id);
+
+        return ResponseEntity.ok()
+                .build();
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id,
+                                                 @RequestBody UpdateArticleRequest request){
+        Article updatedArticle = blogService.update(id,request);
+
+        return ResponseEntity.ok()
+                .body(updatedArticle);
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT Into article (title,content,created_at,updated_at) values ('제목1','내용1',NOW(), NOW())
+INSERT Into article (title,content,created_at,updated_at) values ('제목2','내용2',NOW(), NOW())
+INSERT Into article (title,content,created_at,updated_at) values ('제목3','내용3',NOW(), NOW())
